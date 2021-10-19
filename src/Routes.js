fa69489f24614b2b0f5f30dd8026000b8369b6f7
Jarred Baird <jarredbaird@gmail.com>
2021-10-19 17:47:49 -0400

    need to finish up CreateEventTempateForm

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Authed from "./Authed";
+import CreateEventTemplateForm from "./CreateEventTemplateForm";
 import SignInForm from "./SignInForm.js";
 import SignupForm from "./SignupForm.js";
 
@@ -15,6 +16,9 @@ function Routes() {
       </Route>
       <Route exact path="/authed">
         <Authed />
+      </Route>
+      <Route exact path="/event-template/create">
+        <CreateEventTemplateForm />
       </Route>
       <Redirect to="/" />
     </Switch>
