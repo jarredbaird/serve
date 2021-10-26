e15d5d086ffc6e75bec78dba5ab9c8fd2ba17ef2
Jarred Baird <jarredbaird@gmail.com>
2021-10-26 10:58:14 -0400

    I'm not sure what I did, but I changed something that should prolly be pushed up

--- src/events/CreateEventTemplateForm.js
+++ src/events/CreateEventTemplateForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { useHistory, Link } from "react-router-dom";
 import axios from "axios";
 import "../account/AuthForm.css";
